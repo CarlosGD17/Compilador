7647386d74ca552faa14f21dfc892035d26cc3dd
Leonardo Cortez <leonardocortez389@gmail.com>
2023-06-19 21:59:38 -0600

    Modificando el .bat

--- azul.bat
+++ azul.bat
@@ -1,7 +1,11 @@
 @Echo Off
 cls
 del %1.sal 2> nul
+del %1.asm 2> nul
 java azulAL %1
 if errorlevel 1 goto Fallo
+echo ANALISIS LEXICOGRAFICO CORRECTO
 java azulSLR1 %1
 :Fallo
+ECHO Compilacion terminada
+rem del %1.sal 2> nul
